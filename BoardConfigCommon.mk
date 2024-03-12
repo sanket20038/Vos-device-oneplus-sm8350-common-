3d97040f14ef7d049b760bd6ba02e18d92418096
sanket <sanketapatil2003.com>
2024-03-12 19:21:07 +0000

    error while adding camera igore this commit

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -230,3 +230,5 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/sm8350-common/BoardConfigVendor.mk
+# Camera
+TARGET_USES_OPLUS_CAMERA := true
